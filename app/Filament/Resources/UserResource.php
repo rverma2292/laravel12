8d3eae047376dcbef5d001b04328d76c5d98c17f
rahul-laravel12 <rverma2292@gmail.com>
2026-05-12 18:24:27 +0530

    restrict view mode for user resource

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -86,4 +86,9 @@ class UserResource extends Resource
             'edit' => Pages\EditUser::route('/{record}/edit'),
         ];
     }
+
+    public static function canViewAny(): bool
+    {
+        return auth()->user()->email === 'rahulrnjansrivastav@gmail.com';
+    }
 }
