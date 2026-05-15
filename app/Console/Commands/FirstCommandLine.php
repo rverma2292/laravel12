d30db818a90490eb6c2a92d1738176bbe05722bc
rahul-laravel12 <rverma2292@gmail.com>
2026-05-15 16:27:49 +0530

    First console command

---
+++ app/Console/Commands/FirstCommandLine.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class FirstCommandLine extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:first-command-line';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('This is the first command line');
+        Log::info('This is the first command line to learn');
+        $this->info('Execution end');
+    }
+}
